0a8c4d13b2cad9fe8d081535a6488844fe70ce98
Guilherme <guilherme.gb@hotmail.com>
2024-02-19 19:57:10 -0300

    feat: update users

---
+++ src/app/repositories/user/UpdateRepositories.ts
@@ -0,0 +1,14 @@
+import UserSchema from '../../schemas/UserSchema';
+import { IUser, IUserResponse } from '../../interfaces/iUser';
+
+class UpdateRepositories{
+    async update(id: string, req: IUser):Promise<IUserResponse | null >{
+        return await UserSchema.findByIdAndUpdate(id, req, {
+            new: true,
+            runValidators: true,
+        }).select('-password -_id - decks' ).exec();
+    }
+}
+
+
+export default new UpdateRepositories();
