243b00ba8e295390b80ad54303f349696e2a605d
Guilherme <guilherme.gb@hotmail.com>
2024-02-20 10:09:30 -0300

    feat: delete user repositories

--- src/app/repositories/user/DeleteRepositories.ts
+++ src/app/repositories/user/DeleteRepositories.ts
@@ -1,11 +1,11 @@
 import UserSchema from '../../schemas/UserSchema';
 import { IUserResponse } from '../../interfaces/iUser';
 
-class DeleteRepositories{
+class DeleteUserRepositories{
     async delete(id: string):Promise<IUserResponse | null >{
         return await UserSchema.findByIdAndDelete(id);
     }
 }
 
 
-export default new DeleteRepositories();
+export default new DeleteUserRepositories();
