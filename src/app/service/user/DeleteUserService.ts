63f1d56a7186d7c797ba738b38f3a340d0396cd9
Guilherme <guilherme.gb@hotmail.com>
2024-02-20 10:07:02 -0300

    refactor: rename delete user service

---
+++ src/app/service/user/DeleteUserService.ts
@@ -0,0 +1,18 @@
+import { isValidObjectId } from 'mongoose';
+import { IUserResponse } from '../../interfaces/iUser';
+import DeleteUserRepositories from '../../repositories/user/DeleteRepositories';
+import GetDeckById from '../../repositories/user/GetDeckById';
+
+
+class DeleteUsersService{
+
+    async deleteUser(id: string): Promise<IUserResponse | null>{
+        if (!isValidObjectId(id)) throw console.log('Id not valid');
+        
+        const associatesUserDeck = await GetDeckById.getDeckById(id);
+        if(associatesUserDeck?.decks?.length !== 0) throw console.log('User has decks associated');
+        const users = await DeleteUserRepositories.delete(id);
+        return users;
+    }
+}
+export default new DeleteUsersService();
