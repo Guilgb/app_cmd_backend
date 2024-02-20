fb91b5825e4b09a3a2afc91b8ed7ac3c0bc5392f
Guilherme <guilherme.gb@hotmail.com>
2024-02-20 10:08:21 -0300

    refactor: rename post user controller

--- src/app/controller/user/PostUserController.ts
+++ src/app/controller/user/PostUserController.ts
@@ -1,12 +1,12 @@
 import type { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import UserServiceCreate from '../../service/user/PostService';
+import UserServiceCreate from '../../service/user/PostUserService';
 
 class PostUser {
     async post(req: Request, res: Response){
         try {
             const result = UserServiceCreate.post(req.body);
-            return res.status(StatusCodes.CREATED).json({result});
+            return res.status(StatusCodes.CREATED).json({user: (await result).name});
             
         } catch (error) {
             return res.status(500).json(error);
