5997051c88830da7e5836171fc5242c9fbde32bf
Guilherme <guilherme.gb@hotmail.com>
2024-02-20 10:04:45 -0300

    feat: user delete route

--- src/infra/routes/user.route.ts
+++ src/infra/routes/user.route.ts
@@ -3,6 +3,7 @@ import PostUser from '../../app/controller/user/PostUserController';
 import UserPostValidarion from '../../app/middlewares/validations/UserPostValidarion';
 import GetController from '../../app/controller/user/GetUserController';
 import UpdateController from '../../app/controller/user/UpdateUserController';
+import DeleteUserController from '../../app/controller/user/DeleteUserController';
 
 const router = Router();
 
@@ -10,7 +11,7 @@ router.post('/user', UserPostValidarion, PostUser.post);
 router.get('/user', GetController.get);
 router.put('/user/:userId', UpdateController.update);
 router.patch('/user/:userId');
-router.delete('/user/:userId');
+router.delete('/user/:userId', DeleteUserController.delete);
 
 
 
