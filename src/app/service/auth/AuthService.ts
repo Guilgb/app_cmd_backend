8eb78c8a83879a8200c59b8952f90b0716ef3235
Guilherme <guilherme.gb@hotmail.com>
2024-02-29 15:41:13 -0300

    fix: ip route

--- src/app/service/auth/AuthService.ts
+++ src/app/service/auth/AuthService.ts
@@ -26,7 +26,7 @@ class AutService {
             }
         );
 
-        const result = { access_token: auth, email: payload.email, password: payload.password, _id: payload._id};
+        const result = { access_token: auth, email: payload.email, password: payload.password, _id: user._id};
         return result;
     }
 }
